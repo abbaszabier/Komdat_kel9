efbe1600827da8b49d796d97f8e9540e8457ebca
luthfifachru <60166815+luthfifachru@users.noreply.github.com>
2021-03-17 00:09:14 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,8 +9,36 @@ $ sudo apt-get install php-mysql
 #Mendownload package Dolibarr
 $ wget -c https://sourceforge.net/projects/dolibarr/files/Dolibarr%20installer%20for%20Debian-Ubuntu%20%28DoliDeb%29/13.0.1/dolibarr_13.0.1-4_all.deb/download -O dolibarr_13.0.1-4_all.deb
 
-#Menjalankan MySQL
-$ sudo service mysql start
+#Mengekstrak file zip ke dalam folder webroot yang kita inginkan. (Pada kali ini digunakan path /var/www/html/dolibarr)
+$ sudo unzip dolibarr_13.0.1-4_all.zip -d /var/www/html/dolibarr
+
+#Membuat file php kosong untuk config dolibarr nya dan ubah kepemilikan ke user www-data
+$ sudo touch /var/www/html/dolibarr/dolibarr-13.0.1/htdocs/conf/conf.php
+$ sudo chown -R www-data:www-data /var/www/html/dolibarr
+
+#Merestart Service Apache dan MySql
+$ sudo service apache2 restart ; sudo service mysql restart
+
+#Mengkonfigurasi Apache web server
+$ sudo a2enmod rewrite
+$ sudo touch /etc/apache2/sites-available/dolibarr.conf
+$ sudo ln -s /etc/apache2/sites-available/dolibarr.conf /etc/apache2/sites-enabled/dolibarr.conf
+$ sudo nano /etc/apache2/sites-available/dolibarr.conf
+
+<VirtualHost *:80>
+ServerAdmin admin@your-domain.com
+DocumentRoot /var/www/html/dolibarr/
+ServerName your-domain.com
+ServerAlias www.your-domain.com
+<Directory /var/www/html/dolibarr/>
+Options FollowSymLinks
+AllowOverride All
+Order allow,deny
+allow from all
+</Directory>
+ErrorLog /var/log/apache2/your-domain.com-error_log
+CustomLog /var/log/apache2/your-domain.com-access_log common
+</VirtualHost>
 
 #Membuat Database baru dan User untuk database
 $ sudo mysql -u root -p
@@ -19,18 +47,8 @@ $ sudo mysql -u root -p
     GRANT ALL PRIVILEGES ON `db1`.* TO 'dolibarr1'@'localhost';
     FLUSH PRIVILEGES;
 
-#Install Dolibarr
-$ sudo dpkg -i dolibarr_13.0.1-4_all.deb
-
-#Jika ada gagal karena dependency, jalankan ini
-$ sudo apt-get install -f
-
-#Change Ownership
-$ sudo chmod 777 /var/www
-
 #Restart service
 $ sudo service apache2 restart ; sudo service mysql restart
-
 
 #Open Dolibarr at 
 localhost/dolibarr
